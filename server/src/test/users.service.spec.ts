1637a2298dd2e9eff71517e6a1c41b779725a5c3
Maurits <mauritsseelen@hotmail.com>
2023-03-16 11:23:16 +0100

    user service tests

--- server/src/test/users.service.spec.ts
+++ server/src/test/users.service.spec.ts
@@ -1,3 +1,4 @@
+import { User } from '../models/user'
 import * as UserService from '../services/users.service'
 
 describe('UserService', () => {
@@ -7,4 +8,43 @@ describe('UserService', () => {
         expect(users.length).toBe(3)
     })
 
+    test('Should get user by id', async () => {
+        const user = await UserService.find(1)
+        expect(user).toBeDefined()
+    })
+
+    test('Should create user', async () => {
+        const newUser: User = {
+            email: "test@test.com",
+            id: 0,
+            name: "test",
+            password: "123",
+            token: ""
+        }
+        const addedUser = await UserService.create(newUser)
+
+        expect(addedUser).toBeDefined()
+        expect(addedUser.name).toBe(newUser.name)
+        expect(addedUser.email).toBe(newUser.email)
+    })
+
+    test('Should update user', async () => {
+        const userUpdate = await UserService.find(1)
+        const newName = "updated_name"
+        userUpdate.name = newName
+
+        const updatedUser = await UserService.update(userUpdate.id, userUpdate) as User
+
+        expect(updatedUser).toBeDefined()
+        expect(updatedUser.name).toBe(newName)
+        expect(updatedUser.id).toBe(userUpdate.id)
+    })
+
+    test('Should remove user', async () => {
+        const usersBeforeRemove = await UserService.findAll()
+        await UserService.remove(1)
+        const usersAfterRemove = await UserService.findAll()
+
+        expect(usersAfterRemove.length).toBe(usersBeforeRemove.length - 1)
+    })
 })
