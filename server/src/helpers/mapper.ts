10a1800a6b6b3762bf3fabbe956c7ee6f3e389f7
Maurits <mauritsseelen@hotmail.com>
2023-03-25 23:17:19 +0100

    add mapper

---
+++ server/src/helpers/mapper.ts
@@ -0,0 +1,13 @@
+import { GameRoom } from "../models/gameRoom";
+import { User } from "../models/user";
+
+export class Mapper {
+
+    static mapToUser(data: any): User {
+        return { name: data.name, password: data.password, id: data.id, email: data.email, token: data.token }
+    }
+
+    static mapToGameroom(data: any): GameRoom {
+        return { id: data.id, name: data.name, users: data.users }
+    }
+}
