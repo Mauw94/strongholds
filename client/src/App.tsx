9104ccd77cb2d8e4e7099a80407624593d88df28
Maurits <mauritsseelen@hotmail.com>
2023-05-01 23:22:39 +0200

    more react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import './App.css'
+import { strongholdsAPI } from './services/strongholds'
 
 function App() {
+    const [strongholds, setStrongholds] = useState<any>([])
+    const [loading, setLoading] = useState<boolean>(false)
+
+    useEffect(() => {
+        async function loadStrongholds() {
+            setLoading(true)
+            try {
+                const data = await strongholdsAPI.get()
+                setStrongholds(data)
+            } catch (e) {
+                console.error(e)
+                setLoading(false)
+            } finally {
+                setLoading(false)
+            }
+        }
+        loadStrongholds()
+    }, [])
+
     return (
-        <h1>Test123</h1>
+        <div>
+            {loading && (
+                <div className="center-page">
+                    <span className="spinner primary"></span>
+                    <p>Loading...</p>
+                </div>
+            )}
+            <blockquote cite="Benjamin Franklin">
+                Tell me and I forget, teach me and I may remember, involve me and I learn.
+            </blockquote>
+        </div>
     )
 }
 
---
+++ client/src/services/strongholds.ts
@@ -0,0 +1,41 @@
+const baseUrl = "http://localhost:7000/"
+const url = baseUrl + "api/strongholds"
+
+function translateStatusToErrorMessage(status: number) {
+    switch (status) {
+        case 401:
+            return 'Not authenticated'
+        case 403:
+            return 'You do not have permission'
+        default:
+            return 'Something went wrong, please try again'
+    }
+}
+
+function checkStatus(response: any) {
+    if (response.ok) {
+        return response;
+    } else {
+        const httpErrorInfo = {
+            status: response.status,
+            statusText: response.statusText,
+            url: response.url,
+        };
+        console.log(`log server http error: ${JSON.stringify(httpErrorInfo)}`);
+
+        let errorMessage = translateStatusToErrorMessage(httpErrorInfo.status);
+        throw new Error(errorMessage);
+    }
+}
+
+const strongholdsAPI = {
+    get() {
+        return fetch(url)
+            .then(checkStatus)
+            .then(data => {
+                console.log(data)
+            })
+    }
+}
+
+export { strongholdsAPI }
